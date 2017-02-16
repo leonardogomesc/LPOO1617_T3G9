228a00b9b6cf7ee8045b5b4596ee4ce56e13e149
up201503708 <up201503708@B03AZ0508.salas.feupsig.fe.up.pt>
2017-02-16 16:24:39 +0000

    Update

--- Iteration1/src/Game.java
+++ Iteration1/src/Game.java
@@ -2,8 +2,25 @@
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char map[][]={{'X','X','X','X','X','X','X','X','X','X'},
+				      {'X',' ',' ',' ','I',' ','X',' ','G','X'},
+				      {'X','X','X',' ','X','X','X',' ',' ','X'},
+				      {'X',' ','I',' ','I',' ','X',' ',' ','X'},
+				      {'X','X','X',' ','X','X','X',' ',' ','X'},
+				      {'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				      {'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				      {'X','X','X',' ','X','X','X','X',' ','X'},
+				      {'X',' ','I',' ','I',' ','X','K',' ','X'},
+				      {'X','X','X','X','X','X','X','X','X','X'}};
+		
+		for(int i1=0;i1<10;i1++){
+			for(int i2=0;i2<10;i2++){
+				System.out.print(map[i1][i2]);
+			}
+			System.out.println("");
+		}
+		
 	}
+	
 
 }
