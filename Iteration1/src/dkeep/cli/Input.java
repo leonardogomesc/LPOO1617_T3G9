0b35aca8ef240f9b1095f23b4e3e09398cc2138a
up201503708 <up201503708@B03AZ0508.salas.feupsig.fe.up.pt>
2017-02-23 14:41:10 +0000

    Added new classes

---
+++ Iteration1/src/dkeep/cli/Input.java
@@ -0,0 +1,17 @@
+package dkeep.cli;
+
+import java.util.Scanner;
+
+public class Input {
+	
+	public String input(){
+	String a;
+	
+	Scanner s = new Scanner(System.in);
+	
+	a= s.nextLine();
+	
+	s.close();
+	return a;
+	}
+}
---
+++ Iteration1/src/dkeep/cli/Output.java
@@ -0,0 +1,16 @@
+package dkeep.cli;
+
+public class Output {
+
+	public void output(){
+		
+		for(int i1=0;i1<10;i1++){
+			for(int i2=0;i2<10;i2++){
+				System.out.print("");
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+		
+	}
+}
