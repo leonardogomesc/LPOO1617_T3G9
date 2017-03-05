784a42f65511949bf2090158bd65a9386f41bfe6
Leonardo <Leonardo@Leonardo-PC>
2017-03-05 22:59:05 +0000

    Update.

--- Iteration1/src/dkeep/test/TestDungeonGameLogic.java
+++ Iteration1/src/dkeep/test/TestDungeonGameLogic.java
@@ -1,22 +1,26 @@
 package dkeep.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import dkeep.logic.Hero;
 import dkeep.logic.Map;
 
-
 public class TestDungeonGameLogic {
-  char map[][]={{'X','X','X','X','X'},
+
+	 char map[][]={{'X','X','X','X','X'},
 				{'X','H',' ','G','X'},
 				{'I',' ',' ',' ','X'},
 				{'I','k',' ',' ','X'},
 				{'X','X','X','X','X'}};
-  
-  int doors[][]={{2,0},{3,0}};
-  int keypos[]={3,1};
-  int guard[][]={{1,3}};
-  int hero[]={1,1};
-  
-  public void testMoveHeroIntoFreeCell(){
+
+int doors[][]={{2,0},{3,0}};
+int keypos[]={3,1};
+int guard[][]={{1,3}};
+int hero[]={1,1};
+
+public void testMoveHeroIntoFreeCell(){
 	  Map m=new Map(map,doors,keypos);
 	  Hero h=new Hero(hero,0);
 	  int CellPosition1[]={1,1};
@@ -26,5 +30,5 @@ public class TestDungeonGameLogic {
 	 
 	  
 	  
-  }
+}
 }
